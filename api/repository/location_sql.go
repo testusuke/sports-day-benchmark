f2d01be3b339c05ae29245041ddb6761b8bea2eb
ReTic0310 <retic0310@gmail.com>
2025-07-10 16:06:52 +0000

    change: レシーバの型の修正

--- api/repository/location_sql.go
+++ api/repository/location_sql.go
@@ -9,37 +9,37 @@ import (
 	"sports-day/api/pkg/errors"
 )
 
-type locationRepository struct{}
+type location struct{}
 
 func NewLocation() Location {
-	return &locationRepository{}
+	return location{}
 }
 
-func (r *locationRepository) Save(ctx context.Context, db *gorm.DB, location *db_model.Location) (*db_model.Location, error) {
-	if err := db.WithContext(ctx).Save(location).Error; err != nil {
+func (r location) Save(ctx context.Context, db *gorm.DB, location *db_model.Location) (*db_model.Location, error) {
+	if err := db.Save(location).Error; err != nil {
 		return nil, errors.Wrap(err)
 	}
 	return location, nil
 }
 
-func (r *locationRepository) Delete(ctx context.Context, db *gorm.DB, id string) (*db_model.Location, error) {
+func (r location) Delete(ctx context.Context, db *gorm.DB, id string) (*db_model.Location, error) {
 	var location db_model.Location
-	if err := db.WithContext(ctx).First(&location, "id = ?", id).Error; err != nil {
+	if err := db.First(&location, "id = ?", id).Error; err != nil {
 		if errors.Is(err, gorm.ErrRecordNotFound) {
 			return nil, errors.ErrLocationNotFound
 		}
 		return nil, errors.Wrap(err)
 	}
 
-	if err := db.WithContext(ctx).Delete(&location).Error; err != nil {
+	if err := db.Delete(&location).Error; err != nil {
 		return nil, errors.Wrap(err)
 	}
 	return &location, nil
 }
 
-func (r *locationRepository) Get(ctx context.Context, db *gorm.DB, id string) (*db_model.Location, error) {
+func (r location) Get(ctx context.Context, db *gorm.DB, id string) (*db_model.Location, error) {
 	var location db_model.Location
-	if err := db.WithContext(ctx).First(&location, "id = ?", id).Error; err != nil {
+	if err := db.First(&location, "id = ?", id).Error; err != nil {
 		if errors.Is(err, gorm.ErrRecordNotFound) {
 			return nil, errors.ErrLocationNotFound
 		}
@@ -48,17 +48,17 @@ func (r *locationRepository) Get(ctx context.Context, db *gorm.DB, id string) (*
 	return &location, nil
 }
 
-func (r *locationRepository) BatchGet(ctx context.Context, db *gorm.DB, ids []string) ([]*db_model.Location, error) {
+func (r location) BatchGet(ctx context.Context, db *gorm.DB, ids []string) ([]*db_model.Location, error) {
 	var locations []*db_model.Location
-	if err := db.WithContext(ctx).Where("id IN ?", ids).Find(&locations).Error; err != nil {
+	if err := db.Where("id IN ?", ids).Find(&locations).Error; err != nil {
 		return nil, errors.Wrap(err)
 	}
 	return locations, nil
 }
 
-func (r *locationRepository) List(ctx context.Context, db *gorm.DB) ([]*db_model.Location, error) {
+func (r location) List(ctx context.Context, db *gorm.DB) ([]*db_model.Location, error) {
 	var locations []*db_model.Location
-	if err := db.WithContext(ctx).Find(&locations).Error; err != nil {
+	if err := db.Find(&locations).Error; err != nil {
 		return nil, errors.Wrap(err)
 	}
 	return locations, nil
