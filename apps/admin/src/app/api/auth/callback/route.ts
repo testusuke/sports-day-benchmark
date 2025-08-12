bcc20b3eb8f6010f2b36f5c2ac5b3912084ba77f
Tetsuya Yamamoto <testusuke@gmail.com>
2025-08-12 22:46:43 +0900

    wip

--- apps/admin/src/app/api/auth/callback/route.ts
+++ apps/admin/src/app/api/auth/callback/route.ts
@@ -1,49 +1,65 @@
-import {NextRequest} from "next/server"
+import { NextRequest } from "next/server";
+import { gql } from "@/gql/__generated__";
+import { makeApolloClient } from "@/components/ApolloProvider";
+
+const query = gql(`
+  mutation Login($code: String!, $redirect_uri: String!) {
+    login(input: { code: $code, redirectURL: $redirect_uri }) {
+      token
+    }
+  }
+`);
 
 export async function POST(request: NextRequest) {
-    //  form data
-    const form = await request.formData()
-    //  get state and code
-    const code = form.get('code')
+  //  form data
+  const form = await request.formData();
+  //  get state and code
+  const code = form.get("code");
+  //  redirect uri
+  const redirectUri = process.env.NEXT_PUBLIC_OIDC_REDIRECT_URL;
 
-    //  pass code to the backend
-    const backendURL = process.env.NEXT_PUBLIC_API_INTERNAL_URL ? process.env.NEXT_PUBLIC_API_INTERNAL_URL : process.env.NEXT_PUBLIC_API_URL
-    const loginEndpoint = backendURL + '/login'
+  //  post code to the backend using fetch
+  const client = makeApolloClient();
+  const { data, errors } = await client.mutate({
+    mutation: query,
+    variables: {
+      code: code as string,
+      redirect_uri: redirectUri as string,
+    },
+  });
 
-    //  redirect uri
-    const redirectUri = process.env.NEXT_PUBLIC_OIDC_REDIRECT_URL
+  console.log(data, errors);
 
-    //  post code to the backend using fetch
-    const response = await fetch(loginEndpoint, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-            code: code,
-            redirect_uri: redirectUri
-        }),
-    })
-
-    //  get cookie from response
-    const cookie = response.headers.get('set-cookie')
-    const subDirectory = process.env.SUB_DIRECTORY ? process.env.SUB_DIRECTORY : "/"
-    if (cookie) {
-        // redirect to root page
-        return new Response(null, {
-            status: 301,
-            headers: {
-                "Location": subDirectory,
-                "Set-Cookie": cookie,
-            },
-        })
-    }
-
-    //  redirect to root page
+  if (errors) {
     return new Response(null, {
-        status: 301,
-        headers: {
-            "Location": subDirectory,
-        },
-    })
+      status: 400,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  }
+
+  //  get cookie from response
+  const cookie = data?.login.token;
+  const subDirectory = process.env.SUB_DIRECTORY
+    ? process.env.SUB_DIRECTORY
+    : "/";
+  if (cookie) {
+    // redirect to root page
+    return new Response(null, {
+      status: 301,
+      headers: {
+        Location: subDirectory,
+        "Set-Cookie": cookie,
+      },
+    });
+  }
+
+  //  redirect to root page
+  return new Response(null, {
+    status: 301,
+    headers: {
+      Location: subDirectory,
+    },
+  });
 }
